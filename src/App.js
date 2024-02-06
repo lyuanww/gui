6c0f12329f896d243c0cbeedd5f74ebe793c71c3
LiYuan <lywong113@gmail.com>
2024-02-06 22:13:01 +0800

    setup: routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 import './App.css';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
+     <Box sx={{ display: 'flex' }}>
       <Navbar />
+      <Box
+            component="main"
+            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+          >
+            <Toolbar />
+            <Routes>
+            <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </Box>
+      </Box>
     </BrowserRouter>
   );
 }
